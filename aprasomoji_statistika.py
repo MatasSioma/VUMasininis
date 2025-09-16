be0bda30322b1888bb6bc89d8019ffb36751f469
MatasSioma <matassemenas@gmail.com>
2025-09-16 12:29:50 +0300

    Pirmine analize

---
+++ aprasomoji_statistika.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('EKG_pupsniu_analize_subalansuota.csv', sep=';')
+
+columns = list(df.columns) #Visi stulpeliai
+
+for column in ['R_val', 'S_val', 'RR_l_0/RR_l_1', 'signal_mean', 'signal_std', 'seq_size']:
+    print("\n################", column.upper(), "################\n")
+    print(df[column])
+    print('Mediana:' ,df[column].median())
+    print('Moda: ', df[column].mode())
---
+++ get_random.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('EKG_pupsniu_analize.csv', sep=';')
+
+# 1. Žingsnis, atsirinkti duomenys pagal klases 3x500
+df_balanced = df.groupby('label', group_keys=False).apply(lambda x: x.sample(n=500, random_state=42))
+
+df_balanced = df_balanced.reset_index(drop=True)
+
+# 2. Žingsnis, atsirinkti savo pasirinktus požymius
+pozymiai = ['R_val', 'S_val', 'RR_l_0/RR_l_1', 'signal_mean', 'signal_std', 'seq_size', 'label']
+
+df_tik_su_pozymiais = df_balanced[pozymiai]
+
+df_tik_su_pozymiais.to_csv('EKG_pupsniu_analize_subalansuota.csv', index=False, sep=';')
