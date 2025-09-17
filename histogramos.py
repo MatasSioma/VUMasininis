2014a69ad221201837c8abdea87f85e1f38175e6
Tomas178 <tomaspetronis77@gmail.com>
2025-09-17 15:19:42 +0300

    Pasirodo čia histograma

--- histogramos.py
+++ histogramos.py
@@ -6,7 +6,7 @@ df = pd.read_csv('EKG_pupsniu_analize_normalizuota_pagal_minmax.csv', sep=';')
 
 columns = [col for col in df.columns if col.lower() != 'label']
 
-base_dir = 'grafikai/daznio'
+base_dir = 'grafikai/histogramos'
 os.makedirs(base_dir, exist_ok=True)
 
 for col in columns:
@@ -16,5 +16,5 @@ for col in columns:
     plt.ylabel('Dažnis')
     plt.title(f'{col} reikšmių dažnio diagrama')
     plt.tight_layout()
-    plt.savefig(os.path.join(base_dir, f"{col.replace('/', '_')}_hist.png"), dpi=150)
+    plt.savefig(os.path.join(base_dir, f"{col.replace('/', '_')}.png"), dpi=150)
     plt.close()
